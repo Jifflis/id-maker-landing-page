56332badb6438ba9faa2765499138c1d03395559
JeffTyfoom <49195586+Jifflis@users.noreply.github.com>
2025-08-12 17:48:39 +0800

    fix: Remove navigation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
